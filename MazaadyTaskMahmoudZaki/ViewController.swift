6d0d86092945c570452adaf4e900b89f0ad236fb
MahmoudZiko <deep2012_1st@yahoo.com>
2025-04-02 20:49:06 +0200

    Initial Commit

---
+++ MazaadyTaskMahmoudZaki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazaadyTaskMahmoudZaki
+//
+//  Created by Mahmoud Zaki on 02/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MazaadyTaskMahmoudZakiTests/MazaadyTaskMahmoudZakiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MazaadyTaskMahmoudZakiTests.swift
+//  MazaadyTaskMahmoudZakiTests
+//
+//  Created by Mahmoud Zaki on 02/04/2025.
+//
+
+import Testing
+@testable import MazaadyTaskMahmoudZaki
+
+struct MazaadyTaskMahmoudZakiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
